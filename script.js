a25be95a1e45653cd7a35dbb597f8612bc27b55e
QQQQQuin <144788074+QQQQQuin@users.noreply.github.com>
2025-01-28 14:14:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,243 @@
+const board = document.getElementById("board");
+const pause = document.getElementById("pause");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const r_counter = document.getElementById("r_counter");
+const r_clockwise = document.getElementById("r_clockwise");
+
+const a = new Array(21).fill().map(() => new Array(10).fill(0));
+a.push(new Array(10).fill(9));
+// I, J, L, O, S, T, Z
+// const tetrominoes = 
+
+board.style.fontFamily = "monospace";
+
+console.log("2D array filled with zero's is:");
+console.log(a);
+
+// save the falling one to an array?
+let fallingPiece = [];
+let interval = 750;
+
+let gameRunning = true;
+
+function deepCopyArray(arr) {
+    return arr.map(row => Array.isArray(row) ? [...row] : row);
+}
+
+function dropRandom(){
+    let i = Math.floor(Math.random() * 7)+1;
+    console.log(i);
+    // I, J, L, O, S, T, Z
+    if(i==1){
+        a[0][3] = 1;
+        a[0][4] = 1;
+        a[0][5] = 1;
+        a[0][6] = 1;
+
+        fallingPiece = [[0, 3], [0, 4], [0, 5], [0, 6]]
+
+        // (0,3) (0,4) (0,5) (0,6) to fallingPiece?
+    }
+    else if(i==2){
+        a[0][3] = 2;
+        a[1][3] = 2;
+        a[1][4] = 2;
+        a[1][5] = 2;
+
+        fallingPiece = [[0,3],[1,3],[1,4],[1,5]];
+    }
+    else if(i==3){
+        a[0][5] = 3;
+        a[1][3] = 3;
+        a[1][4] = 3;
+        a[1][5] = 3;
+
+        fallingPiece = [[0,5],[1,4],[1,5],[1,3]];
+    }
+    else if(i==4){
+        a[0][4] = 4;
+        a[0][5] = 4;
+        a[1][4] = 4;
+        a[1][5] = 4;
+
+        fallingPiece = [[0,4],[0,5],[1,4],[1,5]];
+    }
+    else if(i==5){
+        a[0][4] = 5;
+        a[0][5] = 5;
+        a[1][3] = 5;
+        a[1][4] = 5;
+
+        fallingPiece = [[0,4],[0,5],[1,3],[1,4]];
+    }
+    else if(i==6){
+        a[0][4] = 6;
+        a[1][3] = 6;
+        a[1][4] = 6;
+        a[1][5] = 6;
+
+        fallingPiece = [[0,4],[1,3],[1,4],[1,5]];
+    }
+    else if(i==7){
+        a[0][3] = 7;
+        a[0][4] = 7;
+        a[1][4] = 7;
+        a[1][5] = 7;
+
+        fallingPiece = [[0,3],[0,4],[1,4],[1,5]];
+    }
+}
+
+function display(){
+    let s = ``;
+    let i = 0;
+    a.forEach(row => {
+        row.forEach(element => {
+            if (element == 0) {
+                s += `   `; // 3 spaces for each 0
+            } else {
+                s += `[${element}]`;
+            }
+        });
+        s += ` ${i++}<br>`; // Add <br> for line break
+    });
+    s += `[-][-][-][-][-][-][-][-][-][-]<br>`;
+    board.innerHTML = s;
+}
+
+function arrayIncludes(arr1, arr2) {
+    // console.log(arr1);
+    // console.log(arr2);
+    for(let i=0;i<4;i++){
+        if(arr1[i][0]==arr2[0] && arr1[i][1]==arr2[1]){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function checkCanFall(){
+    for(let i=0;i<4;i++){
+        if(a[fallingPiece[i][0]+1][fallingPiece[i][1]] != 0){
+            if(!arrayIncludes(fallingPiece, [fallingPiece[i][0]+1, fallingPiece[i][1]])){
+                // console.log("2");
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function checkCanGoLeft(){
+    for(let i=0;i<4;i++){
+        if(fallingPiece[i][1]-1<0){
+            return false
+        }
+        if(a[fallingPiece[i][0]][fallingPiece[i][1]-1] != 0){
+            if(!arrayIncludes(fallingPiece, [fallingPiece[i][0], fallingPiece[i][1]-1])){
+                // console.log("2");
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function checkCanGoRight(){
+    for(let i=0;i<4;i++){
+        if(fallingPiece[i][1]+1>9){
+            return false
+        }
+        if(a[fallingPiece[i][0]][fallingPiece[i][1]+1] != 0){
+            if(!arrayIncludes(fallingPiece, [fallingPiece[i][0], fallingPiece[i][1]+1])){
+                // console.log("2");
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function fall(){
+    if(checkCanFall()){
+        let type = a[fallingPiece[0][0]][fallingPiece[0][1]];
+        // Clear current position in `a`
+        for (let i = 0; i < fallingPiece.length; i++) {
+            let [row, col] = fallingPiece[i];
+            a[row][col] = 0; // Clear the old position
+        }
+
+        // Move `fallingPiece` down and update `a`
+        for (let i = 0; i < fallingPiece.length; i++) {
+            fallingPiece[i][0] += 1; // Move down
+            let [newRow, newCol] = fallingPiece[i];
+            a[newRow][newCol] = type; // Update new position in `a`
+        }
+    }
+}
+
+function update(){
+    if(!checkCanFall()){
+        dropRandom();
+    }
+    fall();
+    display();
+    if(gameRunning == true){
+        setTimeout(update, interval);
+    }
+}
+
+
+display();
+dropRandom();
+console.log(deepCopyArray(fallingPiece));
+display();
+console.log(deepCopyArray(fallingPiece));
+update();
+
+pause.addEventListener("click",() => {
+    gameRunning = !gameRunning;
+    if(gameRunning){
+        update();
+    }
+    console.log(gameRunning);
+})
+left.addEventListener("click",() => {
+    if(checkCanGoLeft()){
+        let type = a[fallingPiece[0][0]][fallingPiece[0][1]];
+        // Clear current position in `a`
+        for (let i = 0; i < fallingPiece.length; i++) {
+            let [row, col] = fallingPiece[i];
+            a[row][col] = 0; // Clear the old position
+        }
+
+        // Move `fallingPiece` down and update `a`
+        for (let i = 0; i < fallingPiece.length; i++) {
+            fallingPiece[i][1] -= 1; // Move left
+            let [newRow, newCol] = fallingPiece[i];
+            a[newRow][newCol] = type; // Update new position in `a`
+        }
+    }
+    display();
+})
+right.addEventListener("click",() => {
+    if(checkCanGoRight()){
+        let type = a[fallingPiece[0][0]][fallingPiece[0][1]];
+        // Clear current position in `a`
+        for (let i = 0; i < fallingPiece.length; i++) {
+            let [row, col] = fallingPiece[i];
+            a[row][col] = 0; // Clear the old position
+        }
+
+        // Move `fallingPiece` down and update `a`
+        for (let i = 0; i < fallingPiece.length; i++) {
+            fallingPiece[i][1] += 1; // Move right
+            let [newRow, newCol] = fallingPiece[i];
+            a[newRow][newCol] = type; // Update new position in `a`
+        }
+    }
+    display();
+})
+
